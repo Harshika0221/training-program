4b8f8d625df9d6ea291b6d047004626f32418c99
harshika0221 <dahiyaharshika4@gmail.com>
2025-08-13 09:54:51 +0530

    updates

--- day 21/portfolio/vite.config.js
+++ day 21/portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'D:\html tutorial\training-program-\day 21\portfolio'   // <-- IMPORTANT
+  base: '/training-program'  // <-- IMPORTANT
 })
